4e0323e17f7b20ba7dc5e475bd9279dbcb323ad4
Tamara Mayer <110002@fhwn.ac.at>
2020-10-08 17:58:51 +0200

    .

---
+++ Challenge1/Challenge1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://fhwnnetworkprog.azurewebsites.net/");
+
+
+            StudentMessage message = new StudentMessage();
+            message.Name = "Tamara Mayer";
+            message.Message = "Hi ,this is a Message.";
+
+
+            var json = JsonSerializer.Serialize(message);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+            var result = await httpClient.PostAsync("api/receiver", data);
+            var test = result.StatusCode;
+        }
+    }
+}
+
---
+++ Challenge1/Challenge1/StudentMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Challenge1
+{
+    public class StudentMessage
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
